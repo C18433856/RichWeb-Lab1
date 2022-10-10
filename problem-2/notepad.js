647e230cf52d781b24430fa40be784f2fd51bdbe
C18433856 <C18433856@mytudublin.ie>
2022-10-11 00:53:09 +0100

    finished problem 2

--- problem-2/notepad.js
+++ problem-2/notepad.js
@@ -1,81 +1,98 @@
-// Add an element to the list
+counter = 0;  // Used to generate ID's
 
-counter = 0;
-
+// Add a new entry to the list
 function Add() {
+  if(document.getElementById("entry").value == ""){
+    alert("Please enter some text");
+    return;
+  }
   // Create container which will contain the new entry
-  const new_entry = document.createElement("div");
+  const new_entry = document.createElement("article");
   new_entry.id = String(counter);
 
   const element = document.getElementById("list");
   element.appendChild(new_entry);   // Add the container to the entry lists
 
+  // Create container which will hold the text of the entry
+  const new_text = document.createElement("p");
+  new_text.id = String(counter).concat("text");
+  new_entry.appendChild(new_text);
+  new_text.innerHTML = document.getElementById("entry").value;
+
   document.getElementById("entry").value = "";   //empty the input
 
-
-
-  const new_text = document.createElement("div");
-  new_text.id = "text" + String(counter);
-
+  // Create container which will hold the buttons of the entry
   const buttons = document.createElement("div");
-  buttons.id = "buttons" + String(Counter);
-  element.appendChild(buttons);
+  buttons.id = String(counter).concat("buttons");
+  new_entry.appendChild(buttons);
 
+  // Create the delete button
   const delete_button = document.createElement("button");
   delete_button.innerHTML = "Delete";
   delete_button.name = String(counter);
   buttons.appendChild(delete_button);
 
+  // Delete button functionality
   delete_button.onclick = function () {
-
     document.getElementById(this.name).remove();
     
   };
 
+  // Create the edit button
   const edit_button = document.createElement("button");
   edit_button.innerHTML = "Edit";
-  delete_button.name = String(counter);
+  edit_button.name = String(counter).concat("text");
+  buttons.appendChild(edit_button);
+
+  // Edit button functionality
   edit_button.onclick = function () {
     const new_text = prompt("Please enter new entry");
     document.getElementById(this.name).innerHTML =  new_text;
   }
-  buttons.appendChild(edit_button);
+
 
   // button to change background to red
   const red = document.createElement("button");
   red.innerHTML = "red";
-  red.name = String(counter);
+  red.name = String(counter).concat("text");
+  buttons.appendChild(red);
+
+  // Red button functionality
   red.onclick = function(){
     change_color(red.innerHTML, red.name)
   }
-  buttons.appendChild(red);
 
-    // button to change background to blue
+
+  // button to change background to blue
   const blue = document.createElement("button");
   blue.innerHTML = "blue";
-  blue.name = String(counter);
+  blue.name = String(counter).concat("text");
+  buttons.appendChild(blue);
+
+  // Blue button functionality
   blue.onclick = function(){
     change_color(blue.innerHTML, blue.name)
   }
-  buttons.appendChild(blue);
 
-
-    // button to change background to green
+  // button to change background to green
   const green = document.createElement("button");
   green.innerHTML = "green";
-  green.name = String(counter);
+  green.name = String(counter).concat("text");
+  buttons.appendChild(green);
+
+  // Green button functionality
   green.onclick = function(){
     change_color(green.innerHTML, green.name)
   }
-  buttons.appendChild(green);
 
-  counter++;
+
+  counter++;  // Increment the counter to generate a new ID for further contianers
 }
 
-document.getElementById("add").addEventListener("click", Add);
+document.getElementById("add").addEventListener("click", Add);    // Make the add button call the above function
 
 
-
+// Function to change the background color of the text container
 function change_color(color, id){
   document.getElementById(id).style.backgroundColor = color;
 };
